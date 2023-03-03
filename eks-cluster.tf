df3fac44a63149cca3bfe1674cc609a46d334dc2
Anderson Ferreira <andersonferreiracosta@outlook.com>
2023-03-02 21:35:28 -0300

    update

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "19.0.4"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.24"
+  cluster_version = "1.25"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
@@ -11,28 +11,39 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
+  }
+  
+  workers_group_defaults = {
+    root_volume_type = "gp3"
   }
 
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
+      name = "node-group"
 
-      instance_types = ["t2.micro"]
+      instance_types = ["t3.micro"]
 
       min_size     = 1
       max_size     = 3
       desired_size = 2
     }
-
-    #    two = {
-    #      name = "node-group-2"
-
-    #      instance_types = ["t3.small"]
-    #
-    #      min_size     = 1
-    #      max_size     = 2
-    #      desired_size = 1
-    #    }
   }
-}
+
+#     fargate_profiles = {
+
+#     nginx = {
+#       selectors = [{ namespace = "default" }]
+#       subnets = module.vpc.private_subnets
+#     }
+    
+#     coredns = {
+#       selectors = [
+#         { 
+#           namespace = "kube-system"
+#           labels    = { "k8s-app" = "kube-dns" }
+#         },        
+#       ]
+#       subnets = module.vpc.private_subnets
+#     }
+# }
+ }
