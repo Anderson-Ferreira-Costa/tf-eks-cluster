be32b7d774858c5d07ae6e1ce12fb511df78c9c6
Anderson Ferreira <andersonferreiracosta@outlook.com>
2023-09-29 12:52:41 -0300

    ok

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
-
-terraform {
-  backend "s3" {
-    bucket  = "anderson-ferreira"
-    key     = "terraform-state/tf-eks-cluster/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "anderson"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket  = "anderson-ferreira"
+#     key     = "terraform-state/tf-eks-cluster/terraform.tfstate"
+#     region  = "us-east-1"
+#     profile = "anderson"
+#   }
+# }
--- eks-cluster.tf
+++ eks-cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "19.0.4"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.22"
+  cluster_version = "1.24"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.public_subnets
@@ -18,9 +18,24 @@ module "eks" {
       name = "node-group-1"
       instance_types = ["t3.micro"]
       min_size     = 1
-      max_size     = 6
-      desired_size = 6
+      max_size     = 5
+      desired_size = 2
+
+    # block_device_mappings = {
+    #     xvda = {
+    #       device_name = "/dev/xvda"
+    #       ebs = {
+    #         volume_size           = 75
+    #         volume_type           = "gp3"
+    #         iops                  = 3000
+    #         throughput            = 150
+    #         encrypted             = true
+    #         delete_on_termination = true
+    #       }
+    #     }
+    #   }
     }
+    
   #  two = {
   #     name = "node-group-2"
   #     instance_types = ["t3.micro"]
